9a7f544ae518427ec67d52232d5e662fefc8ae02
Vyctor <dev.vyctor@gmail.com>
2020-12-17 07:17:40 -0300

    refatorando para sintaxe ES6

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,58 +1,54 @@
-const { expect } = require('chai');
-const calc = require('../src/main.js');
+import { expect } from 'chai';
+import { sum, sub, div, mult } from '../src/main';
 
 describe('Calc Test Suite', () => {
   describe('Smoke Tests', () => {
-    it('should exist the calc lib', () => {
-      expect(calc).to.exist;
+    it('should exist the method `sum`', () => {
+      expect(sum).to.exist;
+      expect(sum).to.be.a('function');
     });
 
     it('should exist the method `sum`', () => {
-      expect(calc.sum).to.exist;
-      expect(calc.sum).to.be.a('function');
+      expect(sub).to.exist;
+      expect(sub).to.be.a('function');
     });
 
     it('should exist the method `sum`', () => {
-      expect(calc.sub).to.exist;
-      expect(calc.sub).to.be.a('function');
+      expect(div).to.exist;
+      expect(div).to.be.a('function');
     });
 
     it('should exist the method `sum`', () => {
-      expect(calc.div).to.exist;
-      expect(calc.div).to.be.a('function');
-    });
-
-    it('should exist the method `sum`', () => {
-      expect(calc.mult).to.exist;
-      expect(calc.mult).to.be.a('function');
+      expect(mult).to.exist;
+      expect(mult).to.be.a('function');
     });
   });
 
   describe('Sum', () => {
     it('should return 4 when `sum(2,2)`', () => {
-      expect(calc.sum(2, 2)).to.be.equal(4);
+      expect(sum(2, 2)).to.be.equal(4);
     });
   });
 
   describe('Sub', () => {
     it('should return 4 when `sub(8,4)`', () => {
-      expect(calc.sub(8, 4)).to.be.equal(4);
+      expect(sub(8, 4)).to.be.equal(4);
     });
   });
 
   describe('Div', () => {
     it('should return 1 when `sub(2,2)`', () => {
-      expect(calc.div(2, 2)).to.be.equal(1);
+      expect(div(2, 2)).to.be.equal(1);
     });
   });
 
   describe('Mult', () => {
     it('should return 4 when `sub(2,2)`', () => {
-      expect(calc.mult(2, 2)).to.be.equal(4);
+      expect(mult(2, 2)).to.be.equal(4);
     });
 
     it('should not divide by zero', () => {
-      expect(calc.div(4, 0)).to.be.equal('Não é possível dividir por zero!');
+      expect(div(4, 0)).to.be.equal('Não é possível dividir por zero!');
     });
   });
 });
